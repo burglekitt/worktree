46f037491ca424bdf5d8093aa5861ded45e77bc3
Baldur Páll Hólmgeirsson <baldurpan@gmail.com>
2026-03-16 10:35:25 +0000

    Format

--- docs/src/components/Footer.test.tsx
+++ docs/src/components/Footer.test.tsx
@@ -23,9 +23,7 @@ describe("Footer", () => {
   it("renders org and maintainer links", () => {
     render(<Footer />);
 
-    expect(
-      screen.getByText(/CLI Version: v\d+\.\d+\.\d+/),
-    ).toBeInTheDocument();
+    expect(screen.getByText(/CLI Version: v\d+\.\d+\.\d+/)).toBeInTheDocument();
     expect(screen.getByText("Maintainers:")).toBeInTheDocument();
 
     const orgLinks = screen.getAllByRole("link", { name: "burglekitt" });
